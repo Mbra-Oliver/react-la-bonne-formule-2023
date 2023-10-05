b51688f4dd18053b2f2d416f46f2843faec28ce5
Mbra Oliver <mbraoliver28@gmail.com>
2023-10-05 23:22:19 +0000

    fix

---
+++ src/components/HouseComponent.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import HouseRoom from "./HouseRoom";
+
+export default function HouseComponent() {
+  const [allAmpouleOn, setAllAmpouleOn] = useState(false);
+
+  const [houseAmpouleState, setHouseAmpouleState] = useState([
+    false,
+    false,
+    false,
+  ]);
+
+  //Fonction pour allumer / éteindre toute les ampoules
+
+  const toggleAllAmpouleState = () => {
+    setAllAmpouleOn(!allAmpouleOn);
+    const newHouseAmpouleState = houseAmpouleState.map(() => allAmpouleOn);
+    setHouseAmpouleState(newHouseAmpouleState);
+  };
+
+  //Fonction pour allumer ou éteindre une ampoule particulière
+
+  const toggleAmpoule = (index) => {
+    const newHouseAmpouleState = [...houseAmpouleState];
+    newHouseAmpouleState[index] = !newHouseAmpouleState[index];
+    setHouseAmpouleState(newHouseAmpouleState);
+  };
+
+  return (
+    <>
+      <div className="ampoule_container">
+        <HouseRoom
+          mode={houseAmpouleState[0]}
+          toggleAmpoule={() => toggleAmpoule(0)}
+        />
+        <HouseRoom
+          mode={houseAmpouleState[1]}
+          toggleAmpoule={() => toggleAmpoule(1)}
+        />
+        <HouseRoom
+          mode={houseAmpouleState[2]}
+          toggleAmpoule={() => toggleAmpoule(2)}
+        />
+      </div>
+
+      <button
+        className="main_button"
+        onClick={toggleAllAmpouleState}
+        style={{
+          backgroundColor: !allAmpouleOn ? "green" : "red",
+        }}
+      >
+        {!allAmpouleOn ? "Allumer" : "Eteindre"} toute les ampoules
+      </button>
+    </>
+  );
+}
---
+++ src/components/HouseRoom.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./room.css";
+
+export default function HouseRoom({ mode, toggleAmpoule }) {
+  return (
+    <div className="single_ampoule_container">
+      <div className={`ampoule_item ${!mode ? "light" : "dark"}`}></div>
+
+      <div>Etat: {!mode ? "Allumé" : "Eteinte"}</div>
+
+      <button
+        onClick={toggleAmpoule}
+        style={{
+          backgroundColor: !mode ? "green" : "red",
+        }}
+      >
+        {!mode ? "Eteindre" : "Allumer"} l'ampoule
+      </button>
+    </div>
+  );
+}
