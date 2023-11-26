a3db0bee48d55306891d02dbf82ff865d282502b
Mbra Oliver <mbraoliver28@gmail.com>
2023-11-26 17:48:14 +0000

    fix

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 export default function ProductItem({ product }) {
-  console.log(product);
-
-  const backgroundImage = {
-    backgroundImage: url(`${product.image}`),
+  const backgroundImageStyle = {
+    backgroundImage: `url(${product.image})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    height: "200px",
   };
 
   return (
     <div className="product-item">
-      <div className="product-image"></div>
+      <div style={backgroundImageStyle}></div>
       <div className="product-name">A Good Product</div>
       <p className="product-desc">{product.description.slice(0, 100)}</p>
 
